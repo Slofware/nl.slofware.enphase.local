e55ef17a3dcd7b2ec244e3bc0e64f779d108dd8b
Slofware <37899935+Slofware@users.noreply.github.com>
2023-03-10 13:21:08 +0100

    Add files via upload

--- drivers/enphase-enlighten/api.ts
+++ drivers/enphase-enlighten/api.ts
@@ -13,151 +13,169 @@ export default class EnlightenApi {
     serial: string;
     hostname: string
     private client: any;
-    private token: string | undefined
+    private token: string = ''
+    private sessionID: string = ''
 
     private constructor(username: string, password: string, serial: string, hostname: string) {
-      this.username = username;
-      this.password = password;
-      this.serial = serial;
-      this.hostname = hostname;
+        this.username = username;
+        this.password = password;
+        this.serial = serial;
+        this.hostname = hostname;
     }
 
-    static async createApi(username: string, password: string, serial: string, hostname: string) {
-      const api = new EnlightenApi(username, password, serial, hostname);
-      await api.CreateClient();
-      return api;
+    static createApi(username: string, password: string, serial: string, hostname: string) {
+        const api = new EnlightenApi(username, password, serial, hostname);
+
+        return api;
     }
 
     static async TestCredentials(username: string, password: string, serial: string) {
-      const loginPayload = { 'user[email]': username, 'user[password]': password };
-      let options = {
-        headers: { 'content-type': 'application/x-www-form-urlencoded' },
-      };
-      const res = await axios.post(ENLIGHTEN_AUTH_JSON_URL, loginPayload, options);
-      const { session_id } = res.data;
-      const tokenPayload = { session_id, serial_num: serial, username };
-      options = {
-        headers: { 'content-type': 'application/json' },
-      };
-      const response = await axios.post(TOKEN_URL, tokenPayload, options);
-      const token = <string>response.data.trim('\n');
-      const parsedToken = this.parseJwt(token);
-      return parsedToken.username === username;
+        const loginPayload = { 'user[email]': username, 'user[password]': password };
+        let options = {
+            headers: { 'content-type': 'application/x-www-form-urlencoded' },
+        };
+        const res = await axios.post(ENLIGHTEN_AUTH_JSON_URL, loginPayload, options);
+        const { session_id } = res.data;
+        const tokenPayload = { session_id, serial_num: serial, username };
+        options = {
+            headers: { 'content-type': 'application/json' },
+        };
+        const response = await axios.post(TOKEN_URL, tokenPayload, options);
+        const token = <string>response.data.trim('\n');
+        const parsedToken = this.parseJwt(token);
+        return parsedToken.username === username;
     }
 
     private async CreateClient() {
-      this.client = axios.create({
-        baseURL: `https://${this.hostname}/`,
-        timeout: 10000,
-        withCredentials: true,
-        headers: {
-          'Content-Type': 'application/json',
-          Accept: 'application/json',
-        },
-        httpsAgent: new https.Agent({
-          rejectUnauthorized: false,
-        }),
-      });
-      // Set that 401 is not a breaking error.
-      this.client.interceptors.response.use((response: any) => {
-        return response;
-      }, (error: { response: { status: number; }; }) => {
-        if (error.response.status === 401) {
-          console.log('Error 401');
-          this.CollectToken();
-        }
-        return error;
-      });
-      if (this.token === '') {
-        await this.CollectToken();
-      }
+        this.client = axios.create({
+            baseURL: `https://${this.hostname}/`,
+            timeout: 10000,
+            withCredentials: true,
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+            },
+            httpsAgent: new https.Agent({
+                rejectUnauthorized: false,
+            }),
+        });
+        // Set that 401 is not a breaking error.
+        this.client.interceptors.response.use((response: any) => {
+            return response;
+        }, (error: { response: { status: number; }; }) => {
+            if (error.response.status === 401) {
+                console.log('Error 401');
+                this.CollectToken();
+            }
+            return error;
+        });
+
     }
 
-    async CollectToken() {
-      return new Promise((resolve, reject) => {
-      // Create basic client
+    CollectToken() {
+
+        // Create basic client
         const client = axios.create({
-          withCredentials: true,
+            withCredentials: true,
         });
         // Get sessionID for later use to get token.
         const loginPayload = { 'user[email]': this.username, 'user[password]': this.password };
         let options = {
-          headers: { 'content-type': 'application/x-www-form-urlencoded' },
+            headers: { 'content-type': 'application/x-www-form-urlencoded' },
         };
         client.post(ENLIGHTEN_AUTH_JSON_URL, loginPayload, options).then((res: AxiosResponse) => {
-          const SessionId = res.data.session_id;
-          const tokenPayload = { session_id: SessionId, serial_num: this.serial, username: this.username };
+            const SessionId = res.data.session_id;
+            const tokenPayload = { session_id: SessionId, serial_num: this.serial, username: this.username };
 
-          options = {
-            headers: { 'content-type': 'application/json' },
-          };
-          client.post(TOKEN_URL, tokenPayload, options).then((response: AxiosResponse) => {
-            const token = <string>response.data.trim('\n');
-            console.log(`Setting token to ${token}`);
-            this.token = token;
-            this.client.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-            resolve(token);
-          });
+            options = {
+                headers: { 'content-type': 'application/json' },
+            };
+            client.post(TOKEN_URL, tokenPayload, options).then((response: AxiosResponse) => {
+                const token = <string>response.data.trim('\n');
+                console.log(`Setting token to ${token}`);
+                this.token = token;
+                this.client.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+            });
         });
-      });
     }
 
-    async GetCookie() {
-      if (this.client.defaults.headers.common['Authorization']) {
+    GetCookie() {
+        this.client.defaults.headers.common['Authorization'] = `Bearer ${this.token}`;
         this.client.get('auth/check_jwt').then((result: any) => {
-          this.client.defaults.headers.common['Cookie'] = result.headers['set-cookie'][0];
+
+            if (result.data.toString().includes("Valid token.")) {
+                this.sessionID = result.headers['set-cookie'][0].substring(10, 42);
+                console.log(`Valid token, sessionId: ${this.sessionID}`);
+
+            }
+            else {
+                this.token = '';
+                this.sessionID = '';
+            }
         });
-      } else {
-        this.CollectToken().then(this.GetCookie);
-      }
     }
 
     static parseJwt(token: any) {
-      const jsonToken = JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString());
-      return jsonToken;
+        const jsonToken = JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString());
+        return jsonToken;
     }
 
-    async checkLogin() {
-      return new Promise<void>((resolve, reject) => {
-        if (this.token === '' || this.token === undefined) {
-          console.log('Token not found, collecting token.');
-          this.CollectToken().then(() => {
-            resolve();
-          });
-        } else {
-          const expiredate: number = (Date.now() - 1000 / 1000);
-          if (EnlightenApi.parseJwt(this.token).exp > expiredate) {
-            console.log('Token expired refreshing');
-            this.CollectToken().then(() => {
-              resolve();
-            });
-          }
-          resolve();
-        }
-      });
-    }
 
     GetData() {
-      return new Promise((resolve, reject) => {
-        this.checkLogin().then(() => {
-          this.client.get('api/v1/production').then((data: { data: object; }) => {
-            resolve(data.data);
-          }).catch((err: any) => {
-            reject(err);
-          });
+
+        return new Promise((resolve, reject) => {
+            this.client = axios.create({
+                baseURL: `https://${this.hostname}/`,
+                timeout: 10000,
+                withCredentials: true,
+                headers: {
+                    'Content-Type': 'application/json',
+                    Accept: 'application/json',
+                },
+                httpsAgent: new https.Agent({
+                    rejectUnauthorized: false,
+                }),
+            });
+
+
+            if (this.token === '') {
+                console.log('Token not found, collecting token.');
+                this.CollectToken();
+            }
+
+            if (this.token !== '') {
+                console.log('Check token');
+                this.GetCookie();
+            }
+
+            if (this.sessionID !== '') {
+                console.log('Get production data');
+                this.client.defaults.headers.common['Cookie'] = `sessionid=${this.sessionID}`;
+                this.client.get('production.json').then((data: { data: object; }) => {
+                    resolve(data.data);
+                    console.log(data.data);
+                }).catch((err: any) => {
+                    reject(err);
+                });
+
+            }
+
         });
-      });
     }
 
+
+    // To do...
     GetInverter() {
-      return new Promise((resolve, reject) => {
-        this.client.get('api/v1/inverters').then((data: { data: object; }) => {
-          console.log(data.data);
-          resolve(data.data);
-        }).catch((err: any) => {
-          reject(err);
+        console.log('Get Inverter...')
+        return new Promise((resolve, reject) => {
+
+            this.client.get('/api/v1/production/inverters/').then((data: { data: object; }) => {
+                resolve(data.data);
+            }).catch((err: any) => {
+                reject(err);
+            });
         });
-      });
     }
 
 }
--- drivers/enphase-enlighten/device.ts
+++ drivers/enphase-enlighten/device.ts
@@ -3,7 +3,7 @@ import EnlightenApi from './api';
 
 class EnphaseDevice extends Homey.Device {
 
-  private interval: number = 1000 * 30
+  private interval: number = 1000 * 60
   api: EnlightenApi | undefined;
 
   /**
@@ -14,21 +14,20 @@ class EnphaseDevice extends Homey.Device {
     if (this.homey.settings.get('client')) {
       this.log('Initialising API');
       const settings = this.homey.settings.get('client');
-      this.api = await EnlightenApi.createApi(settings.username, settings.password, settings.serial, settings.hostname);
+      this.api = EnlightenApi.createApi(settings.username, settings.password, settings.serial, settings.hostname);
       this.homey.setInterval(this.CollectData.bind(this), this.interval);
     } else {
       this.log('API details not found.');
     }
   }
 
-  CollectData() {
-    this.api?.GetData().then((data: any) => {
-      this.setCapabilityValue('meter_power', data.wattHoursLifetime/ 1000).then();
-      this.setCapabilityValue('measure_power', data.wattsNow).then();
-    }).catch((r: any) => {
-    });
-  }
-
+    CollectData() {
+        this.api?.GetData().then((data: any) => {
+            this.setCapabilityValue('meter_power', data.production[0]["whLifetime"] / 1000).then();
+            this.setCapabilityValue('measure_power', data.production[0]["wNow"]).then();
+        }).catch((r: any) => {
+        });
+    }
 }
 
 module.exports = EnphaseDevice;
